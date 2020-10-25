f8f84ce01e38cdee48fa7a509ba01c208eddf93e
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-10-24 19:50:44 -0500

    AAdded new fade-in-pop-in animated component

---
+++ src/animated-components/fade-in-logo/logo.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from "../../logo.svg";
+import { useSpring, animated } from "react-spring";
+
+const App = () => {
+  // Use spring hook takes an object with from and two that species the properties for the
+  // element that is being animated by animated.xyz component
+  // We change the width and opactity of css property to show pop up animation on the image
+  // Refresh the image to see the change
+  const logoAnimation = useSpring({ from: { opacity: 0,width:200 }, opacity: 1,width:500 });
+  return (
+    // The animation is applied by creating the tag with animated addition and style
+    // for the data that we get from the hook usage
+    <React.Fragment>
+      <animated.img
+        src={logo}
+        className="App-logo"
+        alt="logo"
+        style={logoAnimation}
+      />
+      <p>This logo is being animated in. Check the code for knowledge.</p>
+    </React.Fragment>
+  );
+};
+
+export default App;
